97c09e08d018587adac0f82b6dfbf80235382243
Kristian_Koshaliyev <Kristian_Koshaliyev@epam.com>
2018-08-24 18:19:19 +0600

    [IMP] IMPLEMENTED SPRING REST CLIENT

---
+++ src/main/java/client/Application.java
@@ -0,0 +1,28 @@
+package client;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Application {
+	
+	public static void main(String args[]) {
+		SpringApplication.run(Application.class);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+	
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Book book = restTemplate.getForObject("http://localhost:8080/book", Book.class);
+			System.out.println(book);
+		};
+	}
+}
